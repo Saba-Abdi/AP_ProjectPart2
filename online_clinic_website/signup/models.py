364bc5056909f18593bab5cdad43cd86242a353a
Saba-Abdi <sabaabdi924@gmail.com>
2024-01-25 23:09:25 +0330

    Task2 - creating signup and signin interfaces, signup is completed - 3 points

---
+++ online_clinic_website/signup/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.core import validators
+
+
+class User(models.Model):
+    name = models.CharField(max_length=25,  validators=[validators.MinLengthValidator(4), validators.RegexValidator(r'^.*[a-zA-Z].*[\w]*$')])
+    username = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[
+            validators.MinLengthValidator(5),
+            validators.RegexValidator(r'^.*[a-zA-Z].*[\w]*$')
+        ]
+    )
+    password = models.CharField(
+        max_length=50,
+        validators=[
+            validators.MinLengthValidator(8),
+            validators.RegexValidator(r'^\S*$')]
+    )
+    email = models.EmailField()
+    positions = [('secretary', 'secretary'), ('patient', 'patient')]
+    position = models.CharField(max_length=16, choices=positions, default='patient')
+
+    def __str__(self):
+        return self.username + ' ' + self.password
---
+++ online_clinic_website/signup/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import UserForm
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             'Your account is successfully created. Click below to go to the home page.')
+            return redirect('signup_success')
+    else:
+        form = UserForm()
+
+    return render(request, 'signup_success.html', {'form': form})
+
+
+def signup_success(request):
+    return render(request, 'signup_success.html')
