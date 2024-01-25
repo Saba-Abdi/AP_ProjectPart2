5248291206d6af2b50bf616904001132cee8f800
Saba-Abdi <sabaabdi924@gmail.com>
2024-01-25 23:08:34 +0330

    Task1 - creating project and first page interfaces - 3 points

---
+++ online_clinic_website/online_clinic_website/local_settings.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-8t3!$&whob*inf!5s-)ey&lzqa#2bd^h4#n(gsmo6afi#68&75'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
