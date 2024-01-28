d7b0eee141b2aac87ebe0a0e4100ca90a949d273
Saba-Abdi <sabaabdi924@gmail.com>
2024-01-28 14:41:24 +0330

    Task6 - models of database - 1 points

--- online_clinic_website/signin/models.py
+++ online_clinic_website/signin/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Clinic(models.Model):
+    name = models.CharField(max_length=200)
+    capacity = models.IntegerField()
+    cost = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    username = models.CharField(max_length=200)
+    clinic_name = models.CharField(max_length=200)
+    amount_paid = models.FloatField()
+
+
+class CapacityIncrease(models.Model):
+    username = models.CharField(max_length=200)
+    clinic_name = models.CharField(max_length=200)
+    increase_amount = models.IntegerField()
